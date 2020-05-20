f72be4ce0e34ba3762d7fcbca870c693763e434c
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-20 21:09:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-	# 
-	# get '/' do
-	# "<h1>Hello World</h1>"
-	# end
+
+	get "/info" do
+		"Testing the info page"
+	end
 
 	get '/' do
 		erb :index
